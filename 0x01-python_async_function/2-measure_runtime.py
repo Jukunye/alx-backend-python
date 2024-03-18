9f6acac8fc6686a36071ddf61699359990136a1a
Jukunye <jukunyes@gmail.com>
2024-03-18 20:15:07 +0300

    2-measure_runtime.py

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n, max_delay):
+def measure_time(n: int, max_delay: int) -> float:
     """
     Function that returns the runtime of wait_n
     """
---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module contains the function wait_n"""
+
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n, max_delay):
+    """Function that returns the asyncio task"""
+    result = await asyncio.gather(
+        *[task_wait_random(max_delay) for _ in range(n)]
+        )
+    return sorted(result)
